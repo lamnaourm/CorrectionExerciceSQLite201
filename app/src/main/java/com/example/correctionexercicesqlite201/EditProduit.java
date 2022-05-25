9ef9285b37c5aec59935f8c4cfe12691e1994f15
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-25 14:36:15 +0100

    Interface editproduit

--- app/src/main/java/com/example/correctionexercicesqlite201/EditProduit.java
+++ app/src/main/java/com/example/correctionexercicesqlite201/EditProduit.java
@@ -3,12 +3,28 @@ package com.example.correctionexercicesqlite201;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class EditProduit extends AppCompatActivity {
+
+    Spinner sp;
+    EditText elib, efam, epa, epv;
+    MyDBProduit dp;
+    ArrayList<Produit> prds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_produit);
+    }
+
+    public void modifier(View view) {
+    }
+
+    public void supprimer(View view) {
     }
 }
